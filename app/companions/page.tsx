8c5923e11fd6023601ba21153f8998186eac38c7
Vishal Tiwari <vishaltewari2005@gmail.com>
2025-06-30 11:53:40 +0530

    Fix async error

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,33 +1,44 @@
+export const dynamic = 'force-dynamic'
+
 import CompanionCard from "@/components/CompanionCard"
 import SearchInput from "@/components/SearchInput"
 import SubjectFilter from "@/components/SubjectFilter"
 import { getAllCompanions } from "@/lib/actions/companion.action"
 import { getSubjectColor } from "@/lib/utils"
 
-const ComapnionsLibrary = async ({ searchParams }: SearchParams) => {
-  const filters = await searchParams  // /extract data after /companion url
-  const subject = filters.subject ? filters.subject : ''
-  const topic = filters.topic ? filters.topic : ''
+interface SearchParams {
+  searchParams: {
+    subject?: string
+    topic?: string
+  }
+}
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const subject = searchParams.subject || ''
+  const topic = searchParams.topic || ''
 
   const companions = await getAllCompanions({ subject, topic })
-  console.log(companions)
-  // console.log('PARAMS: ',filters);
+
   return (
     <main>
       <section className="flex justify-between gap-4 max-sm:flex-col">
         <h1>Companion Library</h1>
         <div className="flex gap-4">
           <SearchInput />
-          <SubjectFilter/>
+          <SubjectFilter />
         </div>
       </section>
       <section className="companions-grid">
         {companions.map((companion) => (
-          <CompanionCard key={companion.id}{...companion} color={getSubjectColor(companion.subject)} />
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
         ))}
       </section>
     </main>
   )
 }
 
-export default ComapnionsLibrary
+export default CompanionsLibrary
